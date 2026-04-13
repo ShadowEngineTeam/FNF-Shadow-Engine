cab6f43e2c664a0b03c57a1c366f14e2dfcef2aa
Homura <egzozu.be.bas@gmail.com>
2026-04-13 14:03:06 +0300

    stupid transform smh

--- source/engine/objects/CustomSoundTray.hx
+++ source/engine/objects/CustomSoundTray.hx
@@ -180,3 +180,18 @@ class CustomSoundTray extends FlxSoundTray
 			_bars[i].visible = i < globalVolume;
 	}
 }
+
+private class MathTools
+{
+	public static function linearToLog(x:Float, minValue:Float = 0.001):Float
+	{
+		x = Math.max(0, Math.min(1, x));
+		return Math.exp(Math.log(minValue) * (1 - x));
+	}
+
+	public static function logToLinear(x:Float, minValue:Float = 0.001):Float
+	{
+		x = Math.max(minValue, Math.min(1, x));
+		return 1 - (Math.log(x) / Math.log(minValue));
+	}
+}
