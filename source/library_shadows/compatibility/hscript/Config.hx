4d5fb44e0c0db16e1d05cb3aea884d37e5144084
Homura <egzozu.be.bas@gmail.com>
2026-05-23 11:28:12 +0300

    idk

--- source/library_shadows/compatibility/hscript/Config.hx
+++ source/library_shadows/compatibility/hscript/Config.hx
@@ -4,62 +4,50 @@ class Config {
 	// Runs support for custom classes in these
 	public static final ALLOWED_CUSTOM_CLASSES = [
 		"flixel",
-		"flixel.addons",
-		"flixel.animation",
-		"flixel.effects",
-		"flixel.graphics",
-		"flixel.group",
-		"flixel.input",
-		"flixel.math",
-		"flixel.sound",
-		"flixel.text",
-		"flixel.tile",
-		"flixel.tweens",
-		"flixel.ui",
-		"flixel.util",
 		"animate",
-		"engine",
+		#if FEATURE_VIDEOS
+		//"hxvlc",
+		#end
 		"backend",
-		"states",
-		"substates",
-		"objects",
-		"mobile",
-		"psychlua",
 		"cutscenes",
 		"debug",
-		"options",
-		"shaders",
 		"macros",
+		"mobile",
+		"objects",
+		"options",
+		"psychlua",
+		"shaders",
+		"states",
+		"substates",
 	];
 
 	// Runs support for abstract support in these
 	public static final ALLOWED_ABSTRACT_AND_ENUM = [
 		"flixel",
-		"flixel.util",
-		"flixel.math",
-		"flixel.ui",
-		"flixel.input",
-		"flixel.system",
+		"openfl",
+		"haxe.xml",
+		"haxe.CallStack",
 		"animate",
-		"engine",
+		#if FEATURE_VIDEOS
+		//"hxvlc",
+		#end
 		"backend",
-		"states",
-		"substates",
-		"objects",
-		"mobile",
-		"psychlua",
 		"cutscenes",
 		"debug",
-		"options",
-		"shaders",
 		"macros",
+		"mobile",
+		"objects",
+		"options",
+		"psychlua",
+		"shaders",
+		"states",
+		"substates",
 	];
 
 	// Incase any of your files fail
 	// These are the module names
 	public static final DISALLOW_CUSTOM_CLASSES = [
-		"backend.Main",
-		"flixel.graphics.tile.FlxGraphicsShader",
+
 	];
 
 	// Incase any of your files fail
