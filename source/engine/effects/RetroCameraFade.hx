cb3abeac7f90e049a6d02701521031b8648c23a1
Homura <egzozu.be.bas@gmail.com>
2026-06-27 09:09:20 +0300

    jesus

--- source/engine/effects/RetroCameraFade.hx
+++ source/engine/effects/RetroCameraFade.hx
@@ -2,12 +2,46 @@ package effects;
 
 import flixel.util.FlxTimer;
 import flixel.FlxCamera;
+import openfl.filters.BitmapFilter;
 import openfl.filters.ColorMatrixFilter;
 
 class RetroCameraFade
 {
 	// im lazy, but we only use this for week 6
-  	// and also sorta yoinked for djflixel, lol !
+	// and also sorta yoinked for djflixel, lol !
+
+	static function applyFade(camera:FlxCamera, ?fade:ColorMatrixFilter):Void
+	{
+		var arr:Array<BitmapFilter> = [];
+		if (camera.filters != null)
+			for (f in camera.filters)
+				if (!Std.isOfType(f, ColorMatrixFilter)) // keep everything except a previous fade matrix
+					arr.push(f);
+		if (fade != null)
+			arr.push(fade);
+		camera.filters = arr;
+	}
+
+	static inline function whiteMatrix(V:Float):ColorMatrixFilter
+	{
+		return new ColorMatrixFilter([
+			1, 0, 0, 0, V * 255,
+			0, 1, 0, 0, V * 255,
+			0, 0, 1, 0, V * 255,
+			0, 0, 0, 1,       0
+		]);
+	}
+
+	static inline function blackMatrix(V:Float):ColorMatrixFilter
+	{
+		return new ColorMatrixFilter([
+			1, 0, 0, 0, -V * 255,
+			0, 1, 0, 0, -V * 255,
+			0, 0, 1, 0, -V * 255,
+			0, 0, 0, 1,        0
+		]);
+	}
+
 	public static function fadeWhite(camera:FlxCamera, camSteps:Int = 5, time:Float = 1):Void
 	{
 		var steps:Int = 0;
@@ -19,13 +53,7 @@ class RetroCameraFade
 			if (steps == stepsTotal)
 				V = 1;
 
-			var matrix = [
-				1, 0, 0, 0, V * 255,
-				0, 1, 0, 0, V * 255,
-				0, 0, 1, 0, V * 255,
-				0, 0, 0, 1,       0
-			];
-			camera.filters = [new ColorMatrixFilter(matrix)];
+			applyFade(camera, V <= 0 ? null : whiteMatrix(V));
 			steps++;
 		}, stepsTotal + 1);
 	}
@@ -35,13 +63,7 @@ class RetroCameraFade
 		var steps:Int = camSteps;
 		var stepsTotal:Int = camSteps;
 
-		var matrixDerp = [
-			1, 0, 0, 0, 1.0 * 255,
-			0, 1, 0, 0, 1.0 * 255,
-			0, 0, 1, 0, 1.0 * 255,
-			0, 0, 0, 1,         0
-		];
-		camera.filters = [new ColorMatrixFilter(matrixDerp)];
+		applyFade(camera, whiteMatrix(1.0));
 
 		new FlxTimer().start(time / stepsTotal, (_) ->
 		{
@@ -49,13 +71,7 @@ class RetroCameraFade
 			if (steps == stepsTotal)
 				V = 1;
 
-			var matrix = [
-				1, 0, 0, 0, V * 255,
-				0, 1, 0, 0, V * 255,
-				0, 0, 1, 0, V * 255,
-				0, 0, 0, 1,       0
-			];
-			camera.filters = [new ColorMatrixFilter(matrix)];
+			applyFade(camera, V <= 0 ? null : whiteMatrix(V));
 			steps--;
 		}, camSteps);
 	}
@@ -71,13 +87,7 @@ class RetroCameraFade
 			if (steps == stepsTotal)
 				V = 1;
 
-			var matrix = [
-				1, 0, 0, 0, -V * 255,
-				0, 1, 0, 0, -V * 255,
-				0, 0, 1, 0, -V * 255,
-				0, 0, 0, 1,        0
-			];
-			camera.filters = [new ColorMatrixFilter(matrix)];
+			applyFade(camera, V <= 0 ? null : blackMatrix(V));
 			steps++;
 		}, stepsTotal + 1);
 	}
@@ -87,13 +97,7 @@ class RetroCameraFade
 		var steps:Int = camSteps;
 		var stepsTotal:Int = camSteps;
 
-		var matrixDerp = [
-			1, 0, 0, 0, -1.0 * 255,
-			0, 1, 0, 0, -1.0 * 255,
-			0, 0, 1, 0, -1.0 * 255,
-			0, 0, 0, 1,          0
-		];
-		camera.filters = [new ColorMatrixFilter(matrixDerp)];
+		applyFade(camera, blackMatrix(1.0));
 
 		new FlxTimer().start(time / (stepsTotal + 1), (_) ->
 		{
@@ -101,13 +105,7 @@ class RetroCameraFade
 			if (steps == stepsTotal)
 				V = 1;
 
-			var matrix = [
-				1, 0, 0, 0, -V * 255,
-				0, 1, 0, 0, -V * 255,
-				0, 0, 1, 0, -V * 255,
-				0, 0, 0, 1,        0
-			];
-			camera.filters = [new ColorMatrixFilter(matrix)];
+			applyFade(camera, V <= 0 ? null : blackMatrix(V));
 			steps--;
 		}, camSteps + 1);
 	}
